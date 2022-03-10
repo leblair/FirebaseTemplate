ec094756074208a69f846a36b862164f4db48b08
leblair <bryan.a.a7@gmail.com>
2022-03-10 21:04:19 +0100

    arrreglar el recycler de ccomments

--- app/src/main/java/com/example/firebasetemplate/PostDetailFragment.java
+++ app/src/main/java/com/example/firebasetemplate/PostDetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.firebasetemplate;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,19 +11,21 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.firebasetemplate.databinding.CommentsRowBinding;
 import com.example.firebasetemplate.databinding.FragmentPostDetailBinding;
-import com.example.firebasetemplate.databinding.ViewholderPostBinding;
 import com.example.firebasetemplate.model.Comment;
 import com.example.firebasetemplate.model.Post;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 
 public class PostDetailFragment extends AppFragment {
     private Post post;
     private FragmentPostDetailBinding binding;
+    private ArrayList<Comment> comments = new ArrayList<Comment>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,7 +40,7 @@ public class PostDetailFragment extends AppFragment {
 
         String postid = PostDetailFragmentArgs.fromBundle(getArguments()).getPostid();
 
-        db.collection("posts").document(postid).addSnapshotListener((documentSnapshot,error) -> {
+        db.collection("posts").document(postid).addSnapshotListener((documentSnapshot, error) -> {
             post = documentSnapshot.toObject(Post.class);
             binding.contenido.setText(post.content);
             binding.autor.setText(post.authorName);
@@ -45,7 +48,7 @@ public class PostDetailFragment extends AppFragment {
 
             binding.favos.setOnClickListener(v -> {
                 db.collection("posts").document(postid)
-                        .update("likes."+auth.getUid(),
+                        .update("likes." + auth.getUid(),
                                 !post.likes.containsKey(auth.getUid()) ? true : FieldValue.delete());
 
 
@@ -58,7 +61,6 @@ public class PostDetailFragment extends AppFragment {
             });
 
 
-
             binding.favos.setChecked(post.likes.containsKey(auth.getCurrentUser().getUid()));
             binding.numFav.setText(String.valueOf(post.likes.size()));
 
@@ -67,62 +69,78 @@ public class PostDetailFragment extends AppFragment {
                 //add comment
 
                 Comment comment = new Comment();
-                comment.authorName = String.valueOf(auth.getCurrentUser());
-                comment.day = LocalDateTime.now();
+                comment.authorName = auth.getCurrentUser().getEmail();
+//                comment.day = LocalDateTime.now();
                 comment.text = binding.inputComment2.getText().toString();
                 //post.comments.add(comment);
 
-                FirebaseFirestore.getInstance().collection("posts").document(postid).collection("comments").add(comment).addOnCompleteListener(task ->{
+                FirebaseFirestore.getInstance().collection("posts").document(postid).collection("comments").add(comment).addOnCompleteListener(task -> {
                     binding.inputComment.getEditText().setText("");
                 });
 
             });
 
 
-
         });
+
+        FirebaseFirestore.getInstance().collection("posts").document(postid).collection("comments").addSnapshotListener((collectionSnapshot, error1) -> {
+            for (DocumentSnapshot documentSnapshot1 : collectionSnapshot) {
+                Comment comment = documentSnapshot1.toObject(Comment.class);
+                comment.id = documentSnapshot1.getId();
+                comment.postId = postid;
+                comments.add(comment);
+            }
+        });
+        CommentsAdapter commentsAdapter = new CommentsAdapter(comments, getContext());
+        binding.commentsRecycler.setAdapter(commentsAdapter);
     }
 
-    class CommentsAdapter extends RecyclerView.Adapter<PostsHomeFragment.PostsAdapter.ViewHolder> {
+    class CommentsAdapter extends RecyclerView.Adapter<PostDetailFragment.CommentsAdapter.ViewHolder> {
+        private ArrayList<Comment> comments;
+        private Context mContext;
+
+        CommentsAdapter(ArrayList<Comment> comments, Context context) {
+            this.comments = comments;
+            this.mContext = context;
+        }
 
         @NonNull
         @Override
-        public PostsHomeFragment.PostsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return new PostsHomeFragment.PostsAdapter.ViewHolder(ViewholderPostBinding.inflate(getLayoutInflater(), parent, false));
+        public PostDetailFragment.CommentsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new PostDetailFragment.CommentsAdapter.ViewHolder(CommentsRowBinding.inflate(getLayoutInflater(), parent, false));
         }
 
+
         @Override
-        public void onBindViewHolder(@NonNull PostsHomeFragment.PostsAdapter.ViewHolder holder, int position) {
-            Post post = postsList.get(position);
-            holder.binding.contenido.setText(post.content);
-            holder.binding.autor.setText(post.authorName);
-            holder.binding.postLayout.setOnClickListener(v -> {
-                NavGraphDirections.ActionToPostDetailFragment action = NavGraphDirections.actionToPostDetailFragment();
-                action.setPostid(post.postid);
-                navController.navigate(action);
+        public void onBindViewHolder(@NonNull PostDetailFragment.CommentsAdapter.ViewHolder holder, int position) {
+            Comment comment = comments.get(position);
+            db.collection("posts").document(comment.postId).collection("comments").document(comment.id).addSnapshotListener((documentSnapshot, error) -> {
+                holder.binding.autor.setText(comment.authorName);
+                holder.binding.comentario.setText(comment.text);
+    //            holder.binding.fecha.setText(comment.text);
 
+                holder.binding.checkBox2.setOnClickListener(v -> {
+                    db.collection("posts").document(comment.postId).collection("comments").document(comment.id)
+                            .update("likes."+auth.getUid(),
+                                    !comment.likes.containsKey(auth.getUid()) ? true : FieldValue.delete());
+                });
+                holder.binding.numlikes.setText(String.valueOf(comment.likes.size()));
+                holder.binding.checkBox2.setChecked(comment.likes.containsKey(auth.getUid()));
             });
-            Glide.with(requireContext()).load(post.imageUrl).into(holder.binding.imagen);
-
-            holder.binding.favorito.setOnClickListener(v -> {
-                db.collection("posts").document(post.postid)
-                        .update("likes."+auth.getUid(),
-                                !post.likes.containsKey(auth.getUid()) ? true : FieldValue.delete());
-            });
-
-            holder.binding.favorito.setChecked(post.likes.containsKey(auth.getUid()));
         }
 
         @Override
         public int getItemCount() {
-            return postsList.size();
+            return comments.size();
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
-            ViewholderPostBinding binding;
-            public ViewHolder(ViewholderPostBinding binding) {
+            CommentsRowBinding binding;
+
+            public ViewHolder(CommentsRowBinding binding) {
                 super(binding.getRoot());
                 this.binding = binding;
+
             }
         }
     }
--- app/src/main/java/com/example/firebasetemplate/model/Comment.java
+++ app/src/main/java/com/example/firebasetemplate/model/Comment.java
@@ -1,11 +1,12 @@
 package com.example.firebasetemplate.model;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 
 public class Comment {
+    public String id;
     public String authorName;
     public String text;
+    public String postId;
     public HashMap<String, Boolean> likes = new HashMap<>();
-    public LocalDateTime day;
+    //public LocalDateTime day;
 }
